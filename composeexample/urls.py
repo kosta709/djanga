e513f7ff377fe15b0cc8e875198ad2a4173f26d7
Kosta Klevensky <kosta777@gmail.com>
2016-02-07 11:40:51 +0200

    added views.py

--- composeexample/urls.py
+++ composeexample/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
+    url(r'^hello/$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
 ]
